a96da3912bffeebae608e769f7b2c25b06d9ef49
vlasikxd <nobel96@bk.ru>
2023-03-06 02:44:51 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -1,6 +1,6 @@
 package hiber.config;
 
-import hiber.model.User;
+import hiber.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -45,7 +45,7 @@ public class AppConfig {
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
       factoryBean.setHibernateProperties(props);
-      factoryBean.setAnnotatedClasses(User.class);
+      factoryBean.setAnnotatedClasses(User.class, Car.class);
       return factoryBean;
    }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,61 +1,90 @@
 package hiber.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    public Car getCar() {
+        return car;
+    }
 
-   @Column(name = "name")
-   private String firstName;
+    public void setCar(Car car) {
+        this.car = car;
+    }
 
-   @Column(name = "last_name")
-   private String lastName;
+    @OneToOne
+    @MapsId
+    private Car car;
 
-   @Column(name = "email")
-   private String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    @Column(name = "name")
+    private String firstName;
 
-   public Long getId() {
-      return id;
-   }
+    @Column(name = "last_name")
+    private String lastName;
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    @Column(name = "email")
+    private String email;
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public User() {
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public User(String firstName, String lastName, String email, Car car) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.car = car;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
 
-   public String getEmail() {
-      return email;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "car=" + car +
+                ", id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
